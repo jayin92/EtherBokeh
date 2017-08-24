1517ba676ff4badbc06d919d03ea57740814e232
Jay Li <jayin920805@gmail.com>
2017-08-24 22:19:35 +0800

    update

--- eth.py
+++ eth.py
@@ -4,44 +4,64 @@ import time
 
 
 from bokeh.io import curdoc
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, HoverTool
 from bokeh.plotting import Figure, show, output_file, figure
+from bokeh.models.widgets import Slider
 
-# *********************************
-address = '在這裡輸入錢包地址' #e.g. address = '0x72d8B877CD3E0EB4DBF7121d098a816068915433'
-# *********************************
+from datetime import datetime
+
+def multi(List):
+	results = 0
+	for item in List:
+		results += item
+	return results
 
 source = ColumnDataSource(dict(x=[], y=[], avg=[], longavg=[]))
 fig = Figure(x_axis_label = 'Time',
 			 y_axis_label = 'Mhash/s',
+			 x_axis_type = 'datetime',
 			 plot_width=1500,
-			 plot_height=843)
+			 plot_height=800)
 # fig.xaxis.axis_lable = 'Time'
 # fig.yaxis.axis_lable = 'Mhash/s'
 fig.line(source=source, x='x', y='y',
 		 line_width=2, alpha=0.85, color='red', legend='current Hashrate')
 fig.line(source=source, x='x', y='avg',
-		 line_width=2, alpha=0.85, color='blue', legend='average Hashrate(3hr)')
+		 line_width=2, alpha=0.85, color='blue', legend='average Hashrate(3HR)')
 fig.line(source=source, x='x', y='longavg',
-		 line_width=2, alpha=0.85, color='green', legend='average Hashrate(long)')
-api_url = 'http://tweth.tw:8080/api/accounts/' + address
+		 line_width=2, alpha=0.85, color='green', legend='average Hashrate(24HR)')
+
+
+updateTime = 60
+
+
+api_url = 'https://www.tweth.tw/api/accounts/0x9b933392f33eca6ad38e312df615e6a3b0af977d'
 ct = 0
 local = ''
 longHashrate = 0
+fmt = "%Y/%m/%d %H:%M:%S"
+listHash = []
+
 def update():
 	global ct, local, longHashrate
-	ct += 10
-	times = ct / 10
+	if not ct == 1440:
+		ct += 1
+	else:
+		ct = 1440
 	rawdata = requests.get(api_url)
 	data = json.loads(rawdata.text)
 	currentHashrate = data['currentHashrate'] / 1000000
+	if len(listHash) == 1440:
+		listHash.pop(0)
+	listHash.append(currentHashrate)
 	hashrate = data['hashrate'] / 1000000
 	longHashrate = (currentHashrate + longHashrate)
-	local = str(time.strftime("%H:%M:%S"))
-	new_data = dict(x=[ct], y=[currentHashrate], avg=[hashrate], longavg=[longHashrate/times])
-	source.stream(new_data, 10000)
+	local = datetime.now()
+	print(local)
+	new_data = dict(x=[local], y=[currentHashrate], avg=[hashrate], longavg=[multi(listHash)/ct])
+	source.stream(new_data, updateTime * 1000)
 
 curdoc().add_root(fig)
-curdoc().add_periodic_callback(update, 10000)
+curdoc().add_periodic_callback(update, updateTime * 1000)
 
 
